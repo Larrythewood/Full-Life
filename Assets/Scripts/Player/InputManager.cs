a586f8364cee9d9ab2212244426b045d11e68a9d
werwolfy <raphael.nigl@outlook.de>
2025-03-02 14:16:33 +0100

    [TASK] Made playerLook.cs

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -5,19 +5,25 @@ public class InputManager : MonoBehaviour
     private InputSystem_Actions inputActions;
     private InputSystem_Actions.PlayerActions playerActions;
     private PlayerMovement playerMovement;
+    private PlayerLook playerLook;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         inputActions = new InputSystem_Actions();
         playerActions=inputActions.Player;
         playerMovement = GetComponent<PlayerMovement>();
+        playerLook = GetComponent<PlayerLook>();
         playerActions.Jump.performed += ctx => playerMovement.Jump();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         playerMovement.Move(playerActions.Move.ReadValue<Vector2>());
+    }
+    void Update()
+    {
+        playerLook.Look(playerActions.Look.ReadValue<Vector2>());
     }
     private void OnEnable()
     {
